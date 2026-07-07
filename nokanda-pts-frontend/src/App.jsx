9185c2d32d81f7c1332844764165da291918d412
L-nsamba <l.nsamba@alustudent.com>
2026-07-07 12:54:14 +0200

    route protection and chartjs installation

--- nokanda-pts-frontend/src/App.jsx
+++ nokanda-pts-frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import ProtectedRoute from './components/ProtectedRoute'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 import Bookings from './pages/Bookings'
@@ -11,13 +12,45 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route path="/" element={<Login/>}></Route>
-        <Route path="/dashboard" element={<Dashboard/>}></Route>
-        <Route path="/bookings" element={<Bookings/>}></Route>
-        <Route path="/drivers" element={<Drivers/>}></Route>
-        <Route path="/vehicles" element={<Vehicles/>}></Route>
-        <Route path="/pricing" element={<Pricing/>}></Route>
-        <Route path="/notifications" element={<Notifications/>}></Route>
+        
+        <Route path="/dashboard" element={
+          <ProtectedRoute>
+            <Dashboard/>
+          </ProtectedRoute>}>
+        </Route>
+        
+        <Route path="/bookings" element={
+          <ProtectedRoute>
+            <Bookings/>
+          </ProtectedRoute>}>
+        </Route>
+
+        <Route path="/drivers" element={
+          <ProtectedRoute>
+            <Drivers/>
+          </ProtectedRoute>}>
+        </Route>
+
+        <Route path="/vehicles" element={
+          <ProtectedRoute>
+            <Vehicles/>
+          </ProtectedRoute>}>
+        </Route>
+
+        <Route path="/pricing" element={
+          <ProtectedRoute>
+            <Pricing/>
+          </ProtectedRoute>}>
+        </Route>
+
+        <Route path="/notifications" element={
+          <ProtectedRoute>
+            <Notifications/>
+          </ProtectedRoute>}>
+        </Route>
+
       </Routes>
     </BrowserRouter>
   )
