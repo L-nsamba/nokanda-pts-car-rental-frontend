3e98f9d56bdf95b5952d3aab9809f6f71a0dbf68
L-nsamba <l.nsamba@alustudent.com>
2026-07-07 14:45:46 +0200

    api.js creation

---
+++ nokanda-pts-frontend/src/services/api.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const API =  axios.create({
+    baseURL: import.meta.env.VITE_API_URL
+})
+
+// Attaching token to requests
+API.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token')
+    if (token) config.headers.Authorization = `Bearer ${token}`
+    return config
+})
+
+// Redirection back to login when token expires
+API.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401) {
+            localStorage.removeItem('token')
+            window.location.href  = '/'
+        }
+        return Promise.reject(error)
+    }
+)
+
+export const getStats = () => API.get('/admin/stats')
+export const getBookings = () => API.get('/admin/bookings')
+export const getDrivers = () => API.get('/drivers')
+export const getVehicles = () => API.get('/vehicles')
+export const getPricing = () => API.get('/pricing')
+export const getDestinations = () => API.get('/destinations')
+
+export default API
